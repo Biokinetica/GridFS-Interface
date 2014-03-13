5478a069a88ead164f437dc241c5ae971f9f7155
DixieFlatline <dixieflatline@localhost.localdomain>
2014-03-13 11:14:26 -0400

    minor changes

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ GridFS-Interface
 ================
 
 A small wrapper for uploading and downloading a file or multiple files using GridFS.
+
+Dependancies
+============
+
++ libmongoclient
++ boost_filesystem
++ boost_thread
++ cryptopp
--- mongointerface.cpp
+++ mongointerface.cpp
@@ -1,15 +1,3 @@
-// The functions contained in this file are pretty dummy
-// and are included only as a placeholder. Nevertheless,
-// they *will* get included in the static library if you
-// don't remove them :)
-//
-// Obviously, you 'll have to write yourself the super-duper
-// functions to include in the resulting library...
-// Also, it's not necessary to write every function in this file.
-// Feel free to add more files in this project. They will be
-// included in the resulting library.
-
-// A function adding two integers and returning the result
 #include <iostream>
 #include <fstream>
 #include <list>
@@ -160,8 +148,6 @@ bool Uploader::uploadList(list<string> List, string collection)
     meter.reset(total,1,1);
 
     masterList.sort(low_to_high);
-
-    //for(auto derp:masterList) cout << derp.first << endl;
 
     for(auto it:masterList){
 
